0cdb1ef82e22582872289af25c1c928e73f0b5cb
valdaz <valdemar.zavadsky@gmail.com>
2016-10-12 10:59:37 +0200

    Add project files.

---
+++ AzureWebappSSLMutual/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureWebappSSLMutual.MvcApplication" Language="C#" %>
---
+++ AzureWebappSSLMutual/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebappSSLMutual.Startup))]
+namespace AzureWebappSSLMutual
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
